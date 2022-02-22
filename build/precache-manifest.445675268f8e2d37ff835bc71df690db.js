a41613290ddc13b4d9f4d5217a340176d09e8ff2
LOKII840 <hakeemoshomah@outlook.com>
2022-02-23 00:56:21 +0100

    site

---
+++ build/precache-manifest.445675268f8e2d37ff835bc71df690db.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed9a8cbb00f238aaf3a24a1b0222755",
+    "url": "/noterapper/index.html"
+  },
+  {
+    "revision": "f4e799fcba7f02d7139c",
+    "url": "/noterapper/static/js/2.ec7c55b8.chunk.js"
+  },
+  {
+    "revision": "ff3947542b401529d50f",
+    "url": "/noterapper/static/js/main.f46d1582.chunk.js"
+  },
+  {
+    "revision": "3339821b440b222af2fd",
+    "url": "/noterapper/static/js/runtime-main.83450901.js"
+  }
+]);
